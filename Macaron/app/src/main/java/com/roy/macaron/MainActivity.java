c44b1ecd937bb793019a697d20ee00e27be87d50
Roy-Luo <251909755@qq.com>
2016-05-09 10:46:12 +0800

    第一次提交!

---
+++ Macaron/app/src/main/java/com/roy/macaron/MainActivity.java
@@ -0,0 +1,16 @@
+package com.roy.macaron;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/**
+ * For the sake of sylvanas' victory!
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
